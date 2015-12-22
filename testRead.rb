aaf0c40d584ba6502aac35df3ab313c8d119f449
PkuTeaboss <pkuteaboss@gmail.com>
2015-12-22 15:54:04 +0800

    ADD: call 'sh'

--- testRead.rb
+++ testRead.rb
@@ -1,11 +1,19 @@
-
-def task(text)
+def task(text,&cmd)
   puts "Just read a task: #{text}"
+  if cmd != nil
+   puts "Just read a cmd: #{cmd}"
+   cmd.call 
+  end
+end
+
+def sh(cmd)
+  system(cmd)
 end
 
 def desc(text)
-  puts "Just read a desc : #{text}"
+  puts "Just read a desc: #{text}"
 end
 
 
 load 'test.rake'
+
